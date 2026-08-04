fab05b86679e9335ea8d2ddb59ca5172b8fbc82f
DiyaPrajapati1454 <prajapatidiya547@gmail.com>
2026-08-04 17:23:23 +0530

    Connecting with database

---
+++ AngularAuthAPI/Data/Connection.cs
@@ -0,0 +1,19 @@
+﻿using AngularAuthAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularAuthAPI.Data
+{
+    public class Connection : DbContext
+    {
+        public Connection(DbContextOptions<Connection> options): base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<MissionTheme> MissionThemes { get; set; }
+        public DbSet<MissionSkill> MissionSkills { get; set; }
+        public DbSet<Country> Country { get; set; }
+        public DbSet<City> City { get; set; }
+        public DbSet<Missions> Missions { get; set; }
+        public DbSet<MissionApplication> MissionApplications { get; set; }
+        public DbSet<UserDetail> UserDetails { get; set; }
+        public DbSet<UserSkill> UserSkills { get; set; }
+    }
+}
